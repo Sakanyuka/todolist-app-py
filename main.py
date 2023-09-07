63d79cfe2ec53e15cfb589b2d1b1bc80ff80b35b
Sakhile Ndlovu <sakhile979@gmail.com>
2023-09-07 19:55:37 +0200

    added main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# enter todo
+new_todo = input("Enter todo: ")
+# add todo to array list
+todos = []
+todos.append(new_todo)
+#show todo menu
+menu_option = input("MENU\n1. ADD Todo\n2. View Todos: ")
+#check which menu item was selected 
+if menu_option == "1":
+    new_todo = input("Enter todo: ")
+    # add todo to array list
+    todos.append(new_todo)
+    # show todo menu 
+    print("TODOS: ", todos)
+elif menu_option == "2":
+      # show todo menu 
+    print("TODOS: ", todos)
+else:
+   print("Invalid option")
